ab60718fadc88534284b190f7bbae85b39c9b49d
Manuel Montenegro <montenegro@fdi.ucm.es>
2025-02-17 16:58:33 +0100

    Formateado de código

--- semana05/numPares.cpp
+++ semana05/numPares.cpp
@@ -5,33 +5,28 @@
 
 // Cálculo del número de elementos pares de un vector
 
+#include <cassert>
 #include <iostream>
 #include <vector>
-#include <cassert>
 
 using namespace std;
 
 // Calcula el número de elementos pares en el segmento [c..f) del vector v dado.
-int numPares(const vector<int> &v, int c, int f){
-  assert (0 <= c && c <= f && f <= v.size());
+int numPares(const vector<int> &v, int c, int f) {
+  assert(0 <= c && c <= f && f <= v.size());
 
-  if (c == f) {                 // Caso base 1: vector vacío
+  if (c == f) { // Caso base 1: vector vacío
     return 0;
-  } else if (c + 1 == f) {      // Caso base 2: vector unitario
+  } else if (c + 1 == f) { // Caso base 2: vector unitario
     // Si `v[c]` es par, devolvemos 1; si no, devolvemos 0.
     return (v[c] % 2 == 0 ? 1 : 0);
-  } else {                      // Caso recursivo
+  } else { // Caso recursivo
     int m = (c + f) / 2;
     return numPares(v, c, m) + numPares(v, m, f);
-  } 
+  }
 }
-
 
 // Calcula el número de elementos pares en el vector v.
-int numPares(const vector<int> &v) {
-  return numPares(v, 0, v.size());
-}
+int numPares(const vector<int> &v) { return numPares(v, 0, v.size()); }
 
-int main() {
-  cout << numPares({-2, 2, 3, 5, 6, 7, 3, 4}) << '\n';
-}
+int main() { cout << numPares({-2, 2, 3, 5, 6, 7, 3, 4}) << '\n'; }
